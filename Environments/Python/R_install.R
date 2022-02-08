774c33804536e59123c6c848c944b4df06cd9da7
SamueleSoraggi <samuelson1990@gmail.com>
2022-02-08 11:30:05 +0100

    webpage test update

--- Environments/Python/R_install.R
+++ Environments/Python/R_install.R
@@ -1,2 +1,4 @@
 remotes::install_github("theislab/kBET")
 remotes::install_github("satijalab/sctransform", ref="develop")
+#needs installation of r-dtw and r-pheatmap through conda
+remotes::install_github("shenorrLab/cellAlign")
