f0f5db8c128acebbd606522f0c7048f77da84524
SamueleSoraggi <>
2022-02-22 14:02:13 +0100

    update gitignore and notebooks

--- Environments/Python/R_install.R
+++ Environments/Python/R_install.R
@@ -1,4 +1,10 @@
-remotes::install_github("theislab/kBET")
-remotes::install_github("satijalab/sctransform", ref="develop")
+#remotes::install_github("theislab/kBET", lib='sandbox_scRNA_testAndFeedback/scrna-environment/lib/R/library/', force=TRUE)
+
+#r = getOption("repos")
+#r["CRAN"] = "http://cran.us.r-project.org "
+#options(repos = r)
+#install.packages("rlang", lib='sandbox_scRNA_testAndFeedback/scrna-environment/lib/R/library/')
+remotes::install_github("r-lib/rlang", lib='sandbox_scRNA_testAndFeedback/scrna-environment/lib/R/library/', force=TRUE)
+remotes::install_github("satijalab/sctransform", ref="develop", lib='sandbox_scRNA_testAndFeedback/scrna-environment/lib/R/library/', force=TRUE)
 #needs installation of r-dtw and r-pheatmap through conda
-remotes::install_github("shenorrLab/cellAlign")
+#remotes::install_github("shenorrLab/cellAlign", lib='sandbox_scRNA_testAndFeedback/scrna-environment/lib/R/library/')
